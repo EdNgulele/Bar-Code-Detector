d41f6fb5242f39fb9435e8af5003fb6e1a4caf7e
Edilson Ngulele <edilsonngulele@gmail.com>
2018-07-10 23:25:04 +0200

    first commit

---
+++ app/src/main/java/com/example/edblack/barcodedetect/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.edblack.barcodedetect
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
