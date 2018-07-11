87b2e71ee249de047055be70274ca7e19df64d9f
Edilson Ngulele <edilsonngulele@gmail.com>
2018-07-11 23:18:01 +0200

    Connected to firebase and added ML Kit dependecy

--- app/src/main/java/com/example/edblack/barcodedetect/MainActivity.kt
+++ app/src/main/java/com/example/edblack/barcodedetect/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.edblack.barcodedetect
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+import android.util.SparseIntArray
+import android.view.Surface
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+            .setBarcodeFormats(
+                    FirebaseVisionBarcode.FORMAT_AZTEC,
+                    FirebaseVisionBarcode.FORMAT_QR_CODE)
+            .build()
+
+    val detector = FirebaseVision.getInstance()
+            .getVisionBarcodeDetector(options)
+
+
+    private val ORIENTATIONS = SparseIntArray()
+
+
+    //TODO: determine camera rotation
+    //TODO: get and instance of FirebaseVisionBarcodeDetector
+    //TODO: pass the image to the detectInImage method
+
+
+
 }
